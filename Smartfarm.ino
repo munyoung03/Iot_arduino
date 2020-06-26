7b535dcd22821f386d1191419c332aa22c24a697
leemunyoung <lmun768@naver.com>
2020-06-26 09:44:57 +0900

    [ADD] DHT22 ,[DEL] DHT11

--- Smartfarm.ino
+++ Smartfarm.ino
@@ -1,7 +1,6 @@
 #include "WiFiEsp.h"
-#include <DHT11.h>  
+#include "DHT.h" 
 
-// Emulate Serial1 on pins 6/7 if not present
 
 #ifndef HAVE_HWSERIAL1
 
@@ -16,7 +15,7 @@ int soil_humi = A0;
   
 int soil_humi_value;  
 
-DHT11 dht11(pin);
+DHT dht(4, DHT22);  
 
 float temp, humi;
 int i;
@@ -27,46 +26,28 @@ char pass[] = "dgswdgsw";        // your network password
 
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-
-
 char server[] = "munyoung.kro.kr";
 
-
-
-// Initialize the Ethernet client object
-
 WiFiEspClient client;
-
 
 
 void setup()
 
 {
-
-  // initialize serial for debugging
-
   Serial.begin(9600);
 
-  // initialize serial for ESP module
-
-  Serial1.begin(9600);
-
-  // initialize ESP module
+  dht.begin();
 
   WiFi.init(&Serial1);
 
-  // check for the presence of the shield
-
-  if(i = dht11.read(humi, temp) ==0){
-    soil_humi_value = analogRead(soil_humi);
-    soil_humi_value = map(soil_humi_value,550,0,0,100); 
-    Serial.println(temp);
-    Serial.println(soil_humi_value);
-    connectWifi(humi, temp, soil_humi_value);
-  }
-  else{
-    Serial.println("실패");
-  }
+  soil_humi_value = analogRead(soil_humi);
+  soil_humi_value = map(soil_humi_value,550,0,0,100); 
+  temp = dht.readTemperature();
+  humi = dht.readHumidity();
+  Serial.println(temp);
+  Serial.println(soil_humi_value);
+  connectWifi(humi, temp, soil_humi_value);
+  
 }
 
 
@@ -74,14 +55,10 @@ void connectWifi(float humi, float temp, int soil_humi_value){
    if (WiFi.status() == WL_NO_SHIELD) {
 
     Serial.println("WiFi shield not present");
-  
-    // don't continue
 
     while (true);
 
   }
-
-  // attempt to connect to WiFi network
 
   while ( status != WL_CONNECTED) {
 
@@ -89,13 +66,9 @@ void connectWifi(float humi, float temp, int soil_humi_value){
 
     Serial.println(ssid);
 
-    // Connect to WPA/WPA2 network
-
     status = WiFi.begin(ssid, pass);
 
   }
-
-  // you're connected now, so print out the data
 
   Serial.println("You're connected to the network");
 
@@ -105,14 +78,10 @@ void connectWifi(float humi, float temp, int soil_humi_value){
 
   Serial.println("Starting connection to server...");
 
-  // if you get a connection, report back via serial
-
   if (client.connect(server, 3000)) {
 
     Serial.println("Connected to server");
-
-    // Make a HTTP request
-
+    
     client.print("GET /update?temp=");
     client.print(temp);
     client.print("&humi=");
@@ -133,10 +102,6 @@ void loop()
 
 {
   
-  // if there are incoming bytes available
-
-  // from the server, read them and print them
-
   while (client.available()) {
 
     char c = client.read();
@@ -146,9 +111,6 @@ void loop()
   }
 
 
-
-  // if the server's disconnected, stop the client
-
   if (!client.connected()) {
 
     Serial.println();
@@ -157,9 +119,6 @@ void loop()
 
     client.stop();
 
-
-
-    // do nothing forevermore
 
     while (true);
 
@@ -174,25 +133,16 @@ void printWifiStatus()
 
 {
 
-  // print the SSID of the network you're attached to
 
   Serial.print("SSID: ");
 
   Serial.println(WiFi.SSID());
-
-
-
-  // print your WiFi shield's IP address
 
   IPAddress ip = WiFi.localIP();
 
   Serial.print("IP Address: ");
 
   Serial.println(ip);
-
-
-
-  // print the received signal strength
 
   long rssi = WiFi.RSSI();
 
